b114c2ce30ae957a3054230ed789547af46946cd
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-04-05 11:23:33 -0400

    Updates

--- assets/index.939eee35.js
+++ assets/index.939eee35.js
@@ -1,4 +1,4 @@
 const s=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}};s();document.querySelector("#root").innerHTML=`
-  <h1>Cipher</h1>
+  <h1>CIPHER</h1>
   <a href="https://github.com/Laboratoria/bootcamp/blob/main/projects/01-cipher/README.md" target="_blank">Ver README</a>
 `;
