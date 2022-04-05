2a2baaced5ca46c6d68a498a77bfd9ff768b7343
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-04-04 23:14:48 -0400

    Updates

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#root').innerHTML = `
+  <h1>Cipher</h1>
+  <a href="https://github.com/Laboratoria/bootcamp/blob/main/projects/01-cipher/README.md" target="_blank">Ver README</a>
+`
