c491115c42c37b71186c38fb1c938b7aceb93320
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-04-04 22:32:02 -0400

    feat: move files to src folder, also tweak package.json

--- main.js
+++
@@ -1,6 +0,0 @@
-import './style.css'
-
-document.querySelector('#root').innerHTML = `
-  <h1>Cipher</h1>
-  <a href="https://github.com/Laboratoria/bootcamp/blob/main/projects/01-cipher/README.md" target="_blank">Leer README</a>
-`
