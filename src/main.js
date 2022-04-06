86ae7a845fc4aa7e2a5f2b83abdc878010db7207
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-04-06 13:51:44 -0400

    feat(eslint): add ESLint config with Airbnb code style

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './style.css'
+import './style.css';
+
+console.log('Este `console.log` debería tener un warning');
 
 document.querySelector('#root').innerHTML = `
   <h1>Cipher</h1>
   <a href="https://github.com/Laboratoria/bootcamp/blob/main/projects/01-cipher/README.md" target="_blank">Ver README</a>
-`
+`;
